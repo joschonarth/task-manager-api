01ba3f7c024d165dfa34479b54d4cedb8e366970
joschonarth <joschonarth@gmail.com>
2025-02-11 22:12:11 -0300

    chore: setup eslint

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
-import { app } from "./app";
+import { app } from './app'
 
-app.listen({
+app
+  .listen({
     host: '0.0.0.0',
-    port: 3333
-}).then(() => {
+    port: 3333,
+  })
+  .then(() => {
     console.log('HTTP Server Running!')
-})
+  })
